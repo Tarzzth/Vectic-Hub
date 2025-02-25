0bf8e5837ac66a56adc7aabd206c1612bda777d9
Tarzzth <tarzzth7@gmail.com>
2025-02-26 00:50:23 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,6 @@ if placeid == 3101667897 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Tarzzth/Vectic-Hub/refs/heads/main/script.lua"))()
 else
   game.Players.LocalPlayer:Kick("Script not Support Maps")
-  wait(3)
+  wait(5)
   game:Shutdown()
 end
